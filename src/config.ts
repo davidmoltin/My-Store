fe6f9e1032cb9f2b5640baba87c35bae49d0f09f
Shaun Maharaj <39800563+shaunmaharaj@users.noreply.github.com>
2020-09-21 16:54:02 -0400

    Set env vars for config.ts (#64)
    * Set env vars for config.ts
    
    - Set env vars for config.ts
    
    * - prefix REACT_APP_ for CRA

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
 
 export const config = {
-  clientId: '1NgdZlkNCJ5miRkpQ0ly75HOZOFM0rKf2taUujquog',
-  stripeKey: 'pk_test_JwRX4cevuCysIEQrpwcbn3j8',
+  clientId: process.env.REACT_APP_CLIENT_ID || '1NgdZlkNCJ5miRkpQ0ly75HOZOFM0rKf2taUujquog',
+  stripeKey: process.env.REACT_APP_STRIPE_KEY || 'pk_test_JwRX4cevuCysIEQrpwcbn3j8',
   categoryPageSize: 8,
   maxCompareProducts: 4,
-  algoliaAppId: '6MAFUOD23U',
-  algoliaApiKey: 'placeholder',
-  algoliaPlacesAppId: 'plEPUZAA2D2L',
-  algoliaPlacesApiKey: 'placeholder',
-  algoliaIndexName: 'product',
-  compareKeys: [],
-  endpointURL: 'api.moltin.com',
+  algoliaAppId: process.env.REACT_APP_ALGOLIA_APP_ID || '6MAFUOD23U',
+  algoliaApiKey: process.env.REACT_APP_ALGOLIA_API_KEY || 'placeholder',
+  algoliaPlacesAppId: process.env.REACT_APP_ALGOLIA_PLACES_APP_ID || 'plEPUZAA2D2L',
+  algoliaPlacesApiKey: process.env.REACT_APP_ALGOLIA_PLACES_API_KEY || 'placeholder',
+  algoliaIndexName: process.env.REACT_APP_ALGOLIA_INDEX_NAME || 'product',
+  compareKeys: process.env.REACT_APP_COMPARE_KEYS?.split(',') || [],
+  endpointURL: process.env.REACT_APP_ENDPOINT_URL || 'api.moltin.com',
 };
